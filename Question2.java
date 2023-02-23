4f69ef3aa8531f293bc92b320f03e485caa0e0eb
Kailey <93210118+KaileyKrav@users.noreply.github.com>
2023-02-23 17:36:34 -0500

    Create Question2.java

---
+++ Question2.java
@@ -0,0 +1,5 @@
+/*
+ * Do the same thing as question 1 but instead of 25 test scores, 
+ * ask the user if they would like to continue adding scores. 
+ * When finished print the average of the scores. (Use a while loop)
+ */
