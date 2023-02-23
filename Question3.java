1bd6abf5be3410b1758b1af3222a7194e30a3e1f
Kailey <93210118+KaileyKrav@users.noreply.github.com>
2023-02-23 17:38:55 -0500

    Create Question3.java

---
+++ Question3.java
@@ -0,0 +1,14 @@
+/*
+ * A plane has four rows of seats labeled A - D. Each row has four seats numbered 1 - 4. T
+ * The following seats marked w/ x  are already booked:
+ * A: x  2  3  4
+ * B: 1  2  x  x
+ * C: x  x  3  x
+ * D: 1  2  3  4
+ */
+
+/*
+ * Write a program to ask the user to book a flight, use an if…else statement to determine which seats they can book. 
+ * Their whole party wants to sit in the same row.
+ */
+
